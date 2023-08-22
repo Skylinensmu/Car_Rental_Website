58168ac6a325d8a1da2481715e98218532dbefdb
Skylinensmu <136972609+Skylinensmu@users.noreply.github.com>
2023-08-23 00:28:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("changeImage_Pink").addEventListener("click", function() {
+    var imageElement = document.getElementById("Car_Picture");
+    var newSrc = "Asset/Car_Pink.png"; 
+  
+    imageElement.src = newSrc; 
+});
+
+  
+document.getElementById("changeImage_Green").addEventListener("click", function() {
+    var imageElement = document.getElementById("Car_Picture");
+    var newSrc = "Asset/Car_Green.png"; 
+  
+    imageElement.src = newSrc; 
+});
+
+document.getElementById("changeImage_Cyne").addEventListener("click", function() {
+    var imageElement = document.getElementById("Car_Picture");
+    var newSrc = "Asset/Car_Cyne.png"; 
+  
+    imageElement.src = newSrc; 
+});
+
+document.getElementById("changeImage_Blue").addEventListener("click", function() {
+    var imageElement = document.getElementById("Car_Picture");
+    var newSrc = "Asset/Car_Blue.png"; 
+  
+    imageElement.src = newSrc; 
+});
